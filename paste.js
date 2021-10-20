d313a23c73ea53612de74f62d935ac88bbafe5d3
Skylandia <52195906+Skylandia@users.noreply.github.com>
2021-10-20 13:46:23 +1300

    Update paste.js

--- paste.js
+++ paste.js
@@ -1,4 +1,22 @@
 function getTextQuestion()
 {
-	let question = document.getElementsByClassName("questions-text-alignment whiteTextWithShadow question-size-v4")[0].innerText;
+	return document.getElementsByClassName("questions-text-alignment whiteTextWithShadow question-size-v4")[0].innerText;
 }
+
+function formatTextQuestion(textQuestion)
+{
+	return textQuestion.replace('=', '');
+}
+
+function getAnswer(question)
+{
+	return eval(question);
+}
+
+document.onkeydown = function(e)
+{
+	if (e.keyCode == 69)
+	{
+		console.log(getAnswer(formatTextQuestion(getTextQuestion)));
+	}
+};
