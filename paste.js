df1633faf930cb4324ecaa215157c4b3c00aa2ab
Skylandia <52195906+Skylandia@users.noreply.github.com>
2021-10-21 23:02:20 +1300

    Update paste.js

--- paste.js
+++ paste.js
@@ -1,3 +1,16 @@
+const QType = {
+	L1: 1,
+	L2: 2,
+	L3: 3,
+	L4: 4,
+	L5: 5,
+	L6: 6,
+	L7: 7,
+	L8: 8,
+	L9: 9,
+	L10: 10
+};
+
 document.onkeydown = function(e)
 {
 	if (e.keyCode == 69)
@@ -8,47 +21,126 @@ document.onkeydown = function(e)
 
 function answerQuestion()
 {
-	let answer = eval(getFormattedQuestion());
+	let question = getQuestion();
+	let formattedQuestion = format(question);
+	let questionType = getQuestionType(formattedQuestion);
+	console.log(questionType);
+	let answer = getAnswer(questionType, formattedQuestion);
 	inputAnswer(answer);
 }
 
-function getFormattedQuestion()
+function getQuestionType(question)
 {
-	let question = getQuestion();
+	if (question.length == 5 && isNumber(question[0]) && isOperator(question[1]) && isNumber(question[2]) && question[3] == '=' && question[4] == '')
+		return QType.L1;
 	
-	question = question.replaceAll('×', '*');
-	question = question.replaceAll('÷', '/');
-	question = question.replaceAll('Half of', '0.5 *');
 	
-	let parts = question.split(' ');
+	if (question.length == 5 && isNumber(question[0]) && isOperator(question[1]) && question[2] == '' && question[3] == '=' && isNumber(question[4]))
+		return QType.L3;
 	
-	console.log(parts);
 	
-	if (parts[parts.length-1] != '')
-	{
-		parts[2] = parts[0];
-		parts[0] = parts[4];
-		parts[4] = '';
-		parts[1] = oppositeSign(parts[1]);
-	}
+	if (question.length == 7 && isNumber(question[0]) && isOperator(question[1]) && isNumber(question[2]) && isOperator(question[3]) && isNumber(question[4]) && question[5] == '=' && question[6] == '')
+		return QType.L4;
+	if (question.length == 5 && question[0] == 'Half' && question[1] == 'of' && isNumber(question[2]) && question[3] == '=' && question[4] == '')
+		return QType.L4;
 	
-	question = parts.join('');
-	question = question.replaceAll('=', '');
 	
-	return question;
+	console.log('unknown question type: ' + question);
 }
 
-function oppositeSign(sign)
+function getAnswer(questionType, question)
 {
-	switch(sign)
+	switch (questionType)
 	{
-		case '+': return '-';
-		case '-': return '+';
-		case '*': return '/';
-		case '/': return '*';
+		case QType.L1:
+			question[3] = '';
+			question = question.join('');
+			return eval(question);
+			
+		case QType.L2:
+			return;
+			
+		case QType.L3:
+			if (question[1] == '-' || question[1] == '/')
+			{
+				question[2] = question[4];
+				question[4] = '';
+			}
+			else if (question[1] == '+' || question[1] == '*')
+			{
+				if (question[1] == '+')
+					question[1] = '-';
+				else if (question[1] == '*')
+					question[1] = '/';
+				
+				question[2] = question[0];
+				question[0] = question[4];
+				question[4] = '';
+			}
+			
+			question[3] = '';
+			question = question.join('');
+			return eval(question);
+			
+		case QType.L4:
+			if (question[0] == 'Half')
+			{
+				question[0] = '0.5';
+				question[1] = '*';
+				question[3] = '';
+			}
+			else
+			{
+				question[5] = '';
+			}
+			
+			question = question.join('');
+			return eval(question);
+			
+		case QType.L5:
+			return;
+			
+		case QType.L6:
+			return;
+			
+		case QType.L7:
+			return;
+			
+		case QType.L8:
+			return;
+			
+		case QType.L9:
+			return;
+			
+		case QType.L10:
+			return;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function format(question)
+{
+	question = question.replaceAll('×', '*');
+	question = question.replaceAll('÷', '/');
 	
+	return question.split(' ');
+}
+
 function inputAnswer(answer)
 {
 	document.getElementsByClassName('questions-input-adjustment questions-input-width-v3')[0].value = answer;
@@ -59,67 +151,57 @@ function getQuestion()
 	return document.getElementsByClassName('questions-text-alignment whiteTextWithShadow question-size-v4')[0].innerText;
 }
 
-function arrange1(args)
+function isOperator(part)
 {
-	del args[3];
-	return args.join('');
-}
-function arrange2(args)
-{
-	del args[5];
-	return args.join('');
-}
-function arrange3(args)
-{
-	if (args[1] == '/' || args[1] == '-')
+	switch (part)
 	{
-		args[2] = args[4];
-		args[4] = '';
+		case '+': return true;
+		case '-': return true;
+		case '*': return true;
+		case '/': return true;
+		default: return false;
 	}
-	else
-	{
-		args[2] = args[0];
-		args[0] = args[4];
-		args[4] = '';
-		
-		if (args[1] == '+')
-			args[1] = '-';
-		else
-			args[1] = '*';
-	}
-	
-	del args[3];
-	return args.join('');
 }
-function arrange4(args)
+function isNumber(part)
 {
-	
+	return !isNaN(part);
 }
 
 /*
-Question formats
-
+Level 1:
 [a] [operator] [b] = [ans]
+
+Level 2:
+
+Level 3:
 [a] [operator] [b] [operator] [c] = [ans]
 [a] [operator] [ans] = [b]
+
+Level 4:
 Half of [a] = [ans]
-How many minutes is [a] seconds [ans]
-How many hours in [a] minutes [ans]
+
+Level 5:
+How many [minutes/hours] is [a] [seconds/minutes] [ans]
+How many [minutes/seconds] in [a] [hours/minutes] [ans]
 How many minutes in [a] hours [ans]
 [ans] [unit] = [a] [unit]
 [a] [unit] = [ans] [unit]
 
-Operators:
+Level 6:
 
-+
--
-*
-/
 
-Units:
+Level 7:
 
-mm
-cm
-m
+
+Level 8:
+
+
+Level 9:
+
+
+Level 10:
+
+
+
 
 */
